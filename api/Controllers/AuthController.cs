4ee13473472d44c652bf58710595f5d4c72b7f11
Sofya Sidonskaya <sonyasidonya@gmail.com>
2026-03-22 18:31:51 +0700

    added sso

--- api/Controllers/AuthController.cs
+++ api/Controllers/AuthController.cs
@@ -22,7 +22,7 @@ namespace api.Controllers
         }
 
         /// <summary>
-        /// Login
+        /// Login endpoint just in case (don't touch)
         /// </summary>
         [HttpPost("login")]
         [AllowAnonymous]
@@ -32,6 +32,62 @@ namespace api.Controllers
             if (!ModelState.IsValid) return BadRequest(ModelState);
             var result = await _authService.Login(loginDto);
             return Ok(result);
+        }
+
+
+        /// <summary>
+        /// Returns login page
+        /// </summary>
+        [HttpGet("login")]
+        [AllowAnonymous]
+        public IActionResult LoginPage([FromQuery] string redirectUrl)
+        {
+            var html = $@"
+            <html>
+            <body>
+                <h2>SSO Login</h2>
+                <form method='post' action='/auth/login-form'>
+                    <input type='hidden' name='redirectUrl' value='{redirectUrl}' />
+
+                    <label>Email:</label><br/>
+                    <input type='text' name='email' /><br/>
+
+                    <label>Password:</label><br/>
+                    <input type='password' name='password' /><br/><br/>
+
+                    <button type='submit'>Login</button>
+                </form>
+            </body>
+            </html>";
+
+            return Content(html, "text/html");
+        }
+
+
+        /// <summary>
+        /// Login endpoint for sso form (don't touch)
+        /// </summary>
+        [HttpPost("login-form")]
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginForm(
+            [FromForm] string email,
+            [FromForm] string password,
+            [FromForm] string redirectUrl)
+        {
+            try
+            {
+                var loginDto = new LoginDto
+                {
+                    Email = email,
+                    Password = password
+                };
+                var token = await _authService.Login(loginDto);
+                return Redirect($"{redirectUrl}?access_token={token.AccessToken}");
+            }
+            catch
+            {
+                return Unauthorized("Invalid credentials");
+            }
         }
 
         /// <summary>
